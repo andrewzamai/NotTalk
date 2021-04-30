18081692ecd208d2ff5f51cd88d6500559382208
Andrew <andrewzamai@hotmail.com>
2021-04-30 18:49:11 +0200

    AndroidStudioProject created

---
+++ app/src/main/java/it/unipd/dei/esp2021/nottalk/ItemDetailFragment.kt
@@ -0,0 +1,78 @@
+package it.unipd.dei.esp2021.nottalk
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.appbar.CollapsingToolbarLayout
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import it.unipd.dei.esp2021.nottalk.placeholder.PlaceholderContent
+import it.unipd.dei.esp2021.nottalk.databinding.FragmentItemDetailBinding
+
+/**
+ * A fragment representing a single Item detail screen.
+ * This fragment is either contained in a [ItemListFragment]
+ * in two-pane mode (on larger screen devices) or self-contained
+ * on handsets.
+ */
+class ItemDetailFragment : Fragment() {
+
+    /**
+     * The placeholder content this fragment is presenting.
+     */
+    private var item: PlaceholderContent.PlaceholderItem? = null
+
+    lateinit var itemDetailTextView: TextView
+
+    private var _binding: FragmentItemDetailBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        arguments?.let {
+            if (it.containsKey(ARG_ITEM_ID)) {
+                // Load the placeholder content specified by the fragment
+                // arguments. In a real-world scenario, use a Loader
+                // to load content from a content provider.
+                item = PlaceholderContent.ITEM_MAP[it.getString(ARG_ITEM_ID)]
+            }
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentItemDetailBinding.inflate(inflater, container, false)
+        val rootView = binding.root
+
+        binding.toolbarLayout?.title = item?.content
+
+        itemDetailTextView = binding.itemDetail
+        // Show the placeholder content as text in a TextView.
+        item?.let {
+            itemDetailTextView.text = it.details
+        }
+
+        return rootView
+    }
+
+    companion object {
+        /**
+         * The fragment argument representing the item ID that this fragment
+         * represents.
+         */
+        const val ARG_ITEM_ID = "item_id"
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/it/unipd/dei/esp2021/nottalk/ItemDetailHostActivity.kt
@@ -0,0 +1,34 @@
+package it.unipd.dei.esp2021.nottalk
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import it.unipd.dei.esp2021.nottalk.databinding.ActivityItemDetailBinding
+
+class ItemDetailHostActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding = ActivityItemDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_item_detail) as NavHostFragment
+        val navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_item_detail)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+}
