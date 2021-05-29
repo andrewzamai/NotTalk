998d01b2557e4c021564b59883fa656f06698809
Alessandro Manfè <alessandro.manfe98@gmail.com>
2021-05-30 01:09:11 +0200

    Re-style

--- app/src/main/java/it/unipd/dei/esp2021/nottalk/database/UserRelation.kt
+++ app/src/main/java/it/unipd/dei/esp2021/nottalk/database/UserRelation.kt
@@ -2,10 +2,12 @@ package it.unipd.dei.esp2021.nottalk.database
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "userRelation", primaryKeys = ["thisUser","otherUser"])
+@Entity(tableName = "userRelation", indices = [Index(value = ["thisUser", "otherUser"], unique = true)])
 data class UserRelation(
     @ColumnInfo(name="thisUser") val thisUser: String,
-    @ColumnInfo(name="otherUser") val otherUser: String
-)
+    @ColumnInfo(name="otherUser") val otherUser: String){
+    @PrimaryKey(autoGenerate=true) var id: Int? = null
+}
