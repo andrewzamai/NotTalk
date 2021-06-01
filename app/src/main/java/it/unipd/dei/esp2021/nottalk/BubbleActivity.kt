e2bb34f9a9c6c3505cb505cb51c8380c9ebf9212
Filippo Steffenel <filipposteffenel99@gmail.com>
2021-06-01 11:16:23 +0200

    ModifichePerChatBubble

--- app/src/main/java/it/unipd/dei/esp2021/nottalk/BubbleActivity.kt
+++ app/src/main/java/it/unipd/dei/esp2021/nottalk/BubbleActivity.kt
@@ -13,47 +13,45 @@ import androidx.navigation.NavController
 import it.unipd.dei.esp2021.nottalk.databinding.ActivityItemDetailBinding
 import it.unipd.dei.esp2021.nottalk.databinding.BubbleActivityBinding
 
-class BubbleActivity : AppCompatActivity(R.layout.bubble_activity) {
+class BubbleActivity : AppCompatActivity(R.layout.bubble_activity), NavigationController {
 
     // navController
     private lateinit var navController: NavController
-
-
-    /*
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        val id = intent.data?.lastPathSegment?.toLongOrNull() ?: return
-        if (savedInstanceState == null) {
-            supportFragmentManager.commitNow {
-                replace(R.id.container, ChatFragment.newInstance(id, false))
-            }
-        }
-    }
-
-     */
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         Log.d("BUBBLE_TAG", "onCreateBubbleActivity")
 
+        val id = intent.getIntExtra("chatId",0)
+
         val binding = BubbleActivityBinding.inflate(layoutInflater) //ActivityItemDetailBinding is the binding class generated for activity_item_detail.xml layout, .inflate(layoutInflater) does the inflate as setContentView(R.layout.activity_item_detail)
         val view = binding.root //get a reference to the root view
         setContentView(view)  //and make it active on the screen
-
-    }
-
-
-
-    /*
-    override fun openPhoto(photo: Uri) {
-        // In an expanded Bubble, you can navigate between Fragments just like you would normally
-        // do in a normal Activity. Just make sure you don't block onBackPressed().
-        supportFragmentManager.commit {
-            addToBackStack(null)
-            replace(R.id.container, PhotoFragment.newInstance(photo))
+        if (savedInstanceState == null) {
+            supportFragmentManager.commitNow {
+                replace(R.id.container, ItemDetailFragment.newInstanceText(id, false))
+            }
         }
     }
-*/
 
+    override fun openChat(id: Int, prepopulateText: String?) {
+        throw UnsupportedOperationException("BubbleActivity always shows a single chat thread.")
+    }
+
+
+
+    override fun openPhoto(photo: Uri) {
+        supportFragmentManager.commit {
+            addToBackStack(null)
+            replace(R.id.container, ItemDetailFragment.newInstancePhoto(photo))
+        }
+    }
+
+
+    override fun updateAppBar(
+        showContact: Boolean,
+        hidden: Boolean,
+        body: (name: TextView, icon: ImageView) -> Unit
+    ) { }
 }
